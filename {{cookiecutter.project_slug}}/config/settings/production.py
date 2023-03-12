79bb3aa2d3242190de6b96f85ee582a65017d84d
zhumagulovt <zhumagulovtorokeldi@gmail.com>
2023-03-12 12:45:17 +0600

    fix errors

--- {{cookiecutter.project_slug}}/config/settings/production.py
+++ {{cookiecutter.project_slug}}/config/settings/production.py
@@ -1,3 +1,15 @@
 from .base import env
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+
+# django-cors-headers
+# ------------------------------------------------------------------------------
+# https://github.com/adamchainz/django-cors-headers#setup
+INSTALLED_APPS += ["corsheaders"]  # noqa F405 type: ignore
+
+MIDDLEWARE += [
+    "corsheaders.middleware.CorsMiddleware"
+]  # noqa F405 type: ignore
+
+# django-cors-headers
+CORS_ORIGIN_ALLOW_ALL = True
