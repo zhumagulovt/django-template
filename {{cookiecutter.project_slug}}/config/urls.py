bc700ea344ea2602cce0b1f636d4f007f427847a
zhumagulovt <zhumagulovtorokeldi@gmail.com>
2023-03-12 13:40:34 +0600

    fix errors

--- {{cookiecutter.project_slug}}/config/urls.py
+++ {{cookiecutter.project_slug}}/config/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
@@ -35,3 +35,10 @@ if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
+
+    if "debug_toolbar" in settings.INSTALLED_APPS:
+        import debug_toolbar
+
+        urlpatterns = [
+            path("__debug__/", include(debug_toolbar.urls))
+        ] + urlpatterns
